9095ce8ed09c743bebd31e4b4126696421b8eec4
James Boswell <james.boswell@twcable.com>
2017-02-08 21:41:12 -0500

    Fix slides and code to work on play.golang.org with mocked http.Get with a simple sleep Go playground doesn't allow http operations

--- congeturl.go
+++ congeturl.go
@@ -10,29 +10,42 @@ import (
 
 // Lang is programming languages
 type Lang struct {
-	name string
-	url  string
+	name     string
+	url      string
+	loadtime time.Duration
 }
 
+// URLs with mock load times for Go Playground
 var langs = []Lang{
-	Lang{name: "Go", url: "http://golang.org"},
-	Lang{name: "Python", url: "http://python.org"},
-	Lang{name: "Perl", url: "https://www.perl.org/"}}
+	Lang{name: "Go", url: "http://golang.org", loadtime: parse("86.900303ms")},
+	Lang{name: "Python", url: "http://python.org", loadtime: parse("207.981306ms")},
+	Lang{name: "Perl", url: "https://www.perl.org/", loadtime: parse("376.4593ms")}}
 
 func main() {
 	start := time.Now()
 	results := make(chan string)
 	n := 0
+	// START OMIT
+	// inside main()
 	for _, l := range langs {
 		n++
-		go count(l.name, l.url, results)
+		// 'go' keyword starts mockhttpGet in a goroutine
+		// goroutines are 'green' threads that operate concurrently
+		go mockhttpGet(l.name, l.url, l.loadtime, results)
 	}
-
+	// END OMIT
 	for i := 0; i < n; i++ {
 		fmt.Print(<-results)
 	}
-
 	fmt.Println("Total time: ", time.Since(start))
+}
+
+func mockhttpGet(name string, url string, loadtime time.Duration, results chan string) {
+	start := time.Now()
+	// sleep to mock a http.Get
+	// playground security doesn't allow TCP ops
+	time.Sleep(loadtime)
+	results <- fmt.Sprintf("%s\t[%s]\n", name, time.Since(start))
 }
 
 func count(name string, url string, results chan string) {
@@ -45,4 +58,9 @@ func count(name string, url string, results chan string) {
 	n, _ := io.Copy(ioutil.Discard, r.Body)
 	r.Body.Close()
 	results <- fmt.Sprintf("%s\t%d bytes \t[%s]\n", name, n, time.Since(start))
+}
+
+func parse(t string) time.Duration {
+	dur, _ := time.ParseDuration(t)
+	return dur
 }
--- geturl.go
+++ geturl.go
@@ -10,22 +10,30 @@ import (
 
 // Lang is programming languages
 type Lang struct {
-	name string
-	url  string
+	name     string
+	url      string
+	loadtime time.Duration
 }
 
+// URLs with mock load times for Go Playground
 var langs = []Lang{
-	Lang{name: "Go", url: "http://golang.org"},
-	Lang{name: "Python", url: "http://python.org"},
-	Lang{name: "Perl", url: "https://www.perl.org/"}}
+	Lang{name: "Go", url: "http://golang.org", loadtime: parse("86.900303ms")},
+	Lang{name: "Python", url: "http://python.org", loadtime: parse("207.981306ms")},
+	Lang{name: "Perl", url: "https://www.perl.org/", loadtime: parse("376.4593ms")}}
 
 func main() {
 	start := time.Now()
-
 	for _, l := range langs {
-		count(l.name, l.url)
+		mockhttpGet(l.name, l.url, l.loadtime)
 	}
 	fmt.Println("Total time: ", time.Since(start))
+}
+
+func mockhttpGet(name string, url string, loadtime time.Duration) {
+	start := time.Now()
+	// sleep to mock a http.Get, play.golang.org disallows external calls
+	time.Sleep(loadtime)
+	fmt.Printf("%s\t[%s]\n", name, time.Since(start))
 }
 
 func count(name, url string) {
@@ -38,4 +46,9 @@ func count(name, url string) {
 	n, _ := io.Copy(ioutil.Discard, r.Body)
 	r.Body.Close()
 	fmt.Printf("%s\t%d bytes \t[%s]\n", name, n, time.Since(start))
+}
+
+func parse(t string) time.Duration {
+	dur, _ := time.ParseDuration(t)
+	return dur
 }
